dc2a4914e2cb84551d66c64fc4b7dbadb8fc00c0
Alex Carpenter <alex@mightyinthemidwest.com>
2017-05-18 12:00:06 -0400

    Remove files

--- craft/config/local/sample.db.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-  'server' => 'localhost',
-  'user' => 'username',
-  'password' => 'password',
-  'database' => 'database_name'
-);
--- src/javascripts/modules/example.js
+++
@@ -1,7 +0,0 @@
-export function One () {
-  console.log('One')
-}
-
-export function Two () {
-  console.log('Two')
-}
